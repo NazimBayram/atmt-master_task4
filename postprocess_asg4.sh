b0a6ec7090282ff1ed5464f57d4e4d8b1d115472
Nazim <nazim.bayram@uzh.ch>
2020-11-24 17:09:57 +0100

    Initial Commit

---
+++ postprocess_asg4.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+infile=$1
+outfile=$2
+lang=$3
+
+cat $infile | sed -E 's/(@@ )|(@@ ?$)//g' | perl moses_scripts/detruecase.perl | perl moses_scripts/detokenizer.perl -q -l $lang > $outfile
